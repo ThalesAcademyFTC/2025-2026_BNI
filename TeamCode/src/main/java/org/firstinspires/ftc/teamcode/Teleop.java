752ff245020e4e69fa8beb2efe94cc5890c42488
RyanP2005 <ryan.patel@students.thalesacadmey.org>
2025-08-08 17:06:08 -0400

    creates classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.transition.Slide;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Teleop")
+public class Teleop{
+
+
+
+
+
+
+
+
+
+
+}
