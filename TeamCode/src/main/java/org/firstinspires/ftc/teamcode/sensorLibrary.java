6b89bf8689bd7d6474ee492e1f07ff499e1d2368
FTCThales <ryan.leconey28@thalesacademy.org>
2025-08-22 16:57:56 -0400

    color sensor code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorLibrary.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorLibrary.java
@@ -1,8 +1,60 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.hardware.rev.Rev9AxisImu;
+import com.qualcomm.hardware.rev.Rev9AxisImuOrientationOnRobot;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+//import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import java.util.List;
+
+
 public class sensorLibrary {
 
+    public mainLibrary mainLibrary;
 
+    public boolean isColor(String color) {
+        int red = mainLibrary.colorSensor.red();
+        int blue = mainLibrary.colorSensor.blue();
+        int green = mainLibrary.colorSensor.green();
+        int alpha = mainLibrary.colorSensor.alpha();
+
+        switch (color.toLowerCase()) {
+            case "red":
+                return red > green && red > blue;
+            case "blue":
+                return blue > green && blue > red;
+            case "green":
+                return green > blue && green > red;
+            case "white":
+                return alpha >= 1000;
+            default:
+                return false;
+        }
+    }
 
 }
